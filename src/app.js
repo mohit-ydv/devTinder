9ced84eecd99a2d38070012ab5166c5e69549f63
Mohit Yadav <mohityadav@Mohits-MacBook-Air.local>
2025-06-22 22:39:11 +0530

    deleted the unnecessary comments

--- src/app.js
+++ src/app.js
@@ -18,45 +18,6 @@ app.use('/', profileRouter);
 app.use('/', requestRouter);
 app.use('/', userRouter);
 
-// // delete user
-// app.delete('/user', async (req, res) => {
-//     const userId = req.body.userId;
-//     try {
-//         const user = await User.findByIdAndDelete(userId);
-//         res.send("User deleted successfully");
-//     } catch (error) {
-//         res.status(500).json({ message: 'Error fetching user', error: error.message });
-//     }
-// });
-
-// // update user
-// app.patch('/user/:userId', async (req, res) => {
-//     const userId = req.params.userId;
-//     const data = req.body;
-//     try {
-//         const ALLOWED_UPDATES = ['photoUrl', 'about', 'gender', 'skills', 'age'];
-//         const isUpdateAllowed = Object.keys(data).every((key) => ALLOWED_UPDATES.includes(key));
-//         if (!isUpdateAllowed) {
-//             throw new Error("Update not allowed");
-//         }
-//         if (data?.skills.length > 10) {
-//             throw new Error("Skills cannot be more than 10");
-//         }
-//         const user = await User.findByIdAndUpdate({ _id: userId }, data, {
-//             runValidators: true,
-//         });
-//         res.send("User updated successfully");
-//     } catch (error) {
-//         res.status(500).json({ message: 'Error fetching user', error: error.message });
-//     }
-// });
-
-// // Global error handler to catch any unhandled errors
-// app.use((err, req, res, next) => {
-//     console.error('Unhandled error:', err);
-//     res.status(500).json({ message: 'Something went wrong!', error: err.message });
-// });
-
 // Connect to the database
 connectDB().then(() => {
     console.log("Connected to MongoDB successfully");
